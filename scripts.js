7aa0f6de61f7b4dbedb9c9cc4bce659f54070dd5
SMarcel389 <158337939+SMarcel389@users.noreply.github.com>
2025-10-27 23:33:04 +0100

    .

--- scripts.js
+++ scripts.js
@@ -1,31 +1,68 @@
 console.log("hello")
 const board = document.querySelector(".board");
 
+const canvasSize = document.querySelector("#pixels")
+let width = Number(canvasSize.value)
+let resolution = width * width;
 
-const width = 100
-const resolution = width * width;
+const color = document.querySelector(".color")
+let currentColor = color.value || "#000000"
+color.addEventListener("input", changeColor)
 
+let drawing = false
 document.addEventListener("mousedown", () => drawing = true)
 document.addEventListener("mouseup", () => drawing = false)
+
+createBoard()
+
+const resetBtn = document.querySelector(".resetBtn")
+resetBtn.addEventListener("click", resetBoard)
+
+canvasSize.addEventListener("input", updateCanvasSize)
+
+
 
 
 
 function draw(e) {
     console.log(e.currentTarget.className)
     if (drawing === true) {
-    e.currentTarget.style.backgroundColor = "black"
+    e.currentTarget.style.backgroundColor = currentColor
     }
 }
 
-for (let i = 1; i<=resolution; i++) {
-    pixel = document.createElement("div")
-    pixel.setAttribute("class", "pixel");
-    
-    const pixelSize = `calc(600px / ${width} - 1px)`;
-    pixel.style.flex = `0 0 ${pixelSize}`;
+function changeColor(e) {
+    currentColor = e.target.value
 
-    board.appendChild(pixel)
-
-    pixel.addEventListener("mouseover", draw)
 }
+
+function createBoard() {
+    for (let i = 1; i<=resolution; i++) {
+        const pixel = document.createElement("div")
+        pixel.setAttribute("class", "pixel");
+        
+        const pixelSize = `calc(600px / ${width} - 1px)`;
+        pixel.style.flex = `0 0 ${pixelSize}`;
+        
+
+        board.appendChild(pixel)
+
+        pixel.addEventListener("mouseover", draw)
+    }
+}
+
+function resetBoard() {
+    board.replaceChildren();
+    createBoard()
+}
+
+function updateCanvasSize() {
+    width = Number(canvasSize.value)
+    resolution = width * width
+
+    const currentRange = document.querySelector(".currentRange")
+    currentRange.textContent = width + " " + "pixels"
+    resetBoard()
+}
+
 
